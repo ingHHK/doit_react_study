62336d0941e39e3dc1c4830b1288f06588f60519
ingHHK <inghyunho@naver.com>
2022-02-22 23:50:38 +0900

    간단한 컴포넌트 추가하고 화면으로 띄워보기

--- do-it-example/src/App.js
+++ do-it-example/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import TodaysPlan  from './03/TodaysPlan';
+
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <img src="http://www.easyspub.co.kr/images/logo_footer.png"/>
-                <div>안녕하세요</div>
+            <div className="body">
+                <TodaysPlan />
             </div>
         );
     }
