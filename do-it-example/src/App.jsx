4285a882edd1f993d8f3348619ec84a0e8262b52
ingHHK <inghyunho@naver.com>
2022-02-24 00:01:00 +0900

    필수 프로퍼티 사용하기

--- do-it-example/src/App.jsx
+++ do-it-example/src/App.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import BooleanComponent from './03/BooleanComponent';
+import ChildComponent2 from './03/ChildComponent2';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <div>
-          <b>지루할 때:</b>
-          <BooleanComponent bored></BooleanComponent>
-        </div>
-        <div>
-          <b>즐거울 때:</b>
-          <BooleanComponent></BooleanComponent>
-        </div>
+        <ChildComponent2 objValue={{ age: 20 }} requiredStringValue="문자" />
       </div>
     );
   }
