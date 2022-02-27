2d36fe0f2c91098400a0e05b866a527090d5eb68
ingHHK <inghyunho@naver.com>
2022-02-27 20:08:17 +0900

    카운터 프로그램 만들며 생명주기 함수 사용해 보기

--- do-it-example/src/App.jsx
+++ do-it-example/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import LifecycleExample from './03/LifecycleExample';
+import NewCounter from './03/NewCounter';
+import Counter from './03/Counter';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 10 };
+    this.resetCount = this.resetCount.bind(this);
+  }
+  resetCount() {
+    this.setState(({ count }) => ({ count: count + 10 }));
+  }
   render() {
     return (
       <div>
-        <LifecycleExample />
+        <div>
+          <Counter count={this.state.count} />
+        </div>
+        <div>
+          <NewCounter count={this.state.count} />
+        </div>
+        <button onClick={this.resetCount}>{this.state.count + 10}으로 초기화</button>
       </div>
     );
   }
