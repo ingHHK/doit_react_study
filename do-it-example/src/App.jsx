678619fb66318a51610651c00885270ddfb1de19
ingHHK <inghyunho@naver.com>
2022-03-01 14:05:44 +0900

    컴포넌트에서 DOM 이벤트 사용하기

--- do-it-example/src/App.jsx
+++ do-it-example/src/App.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import Counter from './03/Counter2';
+import Counter from './03/Counter3';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 1,
-    };
-  }
-  increaseCount() {
-    this.setState(({ count }) => ({ count: count + 1 }));
-  }
   render() {
-    return <Counter count={this.state.count} onAdd={this.increaseCount.bind(this)} />;
+    return <Counter></Counter>;
   }
 }
 
