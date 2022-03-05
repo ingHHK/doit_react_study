88236ccf7691a354358fe1821419ad7e401220cc
ingHHK <inghyunho@naver.com>
2022-03-06 00:18:19 +0900

    Text 컴포넌트에 스타일 추가하기

--- do-it-example/src/04/Text.jsx
+++ do-it-example/src/04/Text.jsx
@@ -1,10 +1,11 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import withStyles, { css } from './withStyles';
 
 class Text extends PureComponent {
   render() {
-    const { children } = this.props;
-    return <span>{children}</span>;
+    const { children, styles } = this.props;
+    return <span {...css(styles.default)}>{children}</span>;
   }
 }
 
@@ -12,4 +13,9 @@ Text.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-export default Text;
+export default withStyles(({ color, size }) => ({
+  default: {
+    color: color.default,
+    fontSize: size.md,
+  },
+}))(Text);
