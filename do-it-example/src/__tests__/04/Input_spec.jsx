bb786670aa361d1c4f4b4e24df2279e2a7056650
ingHHK <inghyunho@naver.com>
2022-03-07 20:40:24 +0900

    enzyme 라이브러리 설치하고 테스트해 보기

--- do-it-example/src/__tests__/04/Input_spec.jsx
+++ do-it-example/src/__tests__/04/Input_spec.jsx
@@ -6,11 +6,12 @@ import Input from '../../03/Input';
 describe('<Input>', () => {
   it('renders without crashing', () => {
     expect(() => {
-      shallow(<Input name="test_name" />);
+      shallow(<Input />);
     }).not.toThrow();
   });
+
   it('has one element', () => {
-    const wrapper = shallow(<Input name="test_name" />);
+    const wrapper = shallow(<Input />);
     expect(wrapper.length).toEqual(1);
     expect(wrapper).toHaveLength(1);
   });
