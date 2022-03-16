03f77c6c68d9fdb85a77a5f50bd9f93a17870b9a
ingHHK <inghyunho@naver.com>
2022-03-16 23:49:30 +0900

    simulate() 함수로 가상의 이벤트 검사하기

--- do-it-example/src/__tests__/04/Input.test.jsx
+++ do-it-example/src/__tests__/04/Input.test.jsx
@@ -36,4 +36,14 @@ describe('<Input>', () => {
     expect(wrapper.find('.error')).toHaveLength(1);
     expect(wrapper.html()).toContain(expectedErrorMessage);
   });
+
+  it('calls back onChange on input change', () => {
+    const changeStub = jest.fn();
+    const wrapper = shallow(<Input name="test_name" onChange={changeStub} />);
+    expect(changeStub).not.toHaveBeenCalled();
+    const expectedTargetValue = 'updated input';
+    wrapper.find('input').simulate('change', { target: { value: expectedTargetValue } });
+    expect(changeStub).toHaveBeenCalledTimes(1);
+    expect(changeStub).toHaveBeenCalledWith('test_name', expectedTargetValue);
+  });
 });
